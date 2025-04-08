449b16526eca1bf97669ae0078778c9d462f5617
Felipe Oliveira <flipeaz342@gmail.com>
2025-04-08 19:17:39 -0400

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Olá, mundo!");
+import express, { Request, Response } from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
